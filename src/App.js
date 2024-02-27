0f1979051a133f63c152b66c3c01f82cc55c1e07
Albaraazain <albaraazain33@gmail.com>
2024-02-27 05:40:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Navigation from './components/Navigation';
+import About from './components/About';
+import Features from './components/Features';
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation></Navigation>
+      <About></About>
+      <Features></Features>
+      <Pricing></Pricing>
+      <Footer></Footer>
     </div>
   );
 }
